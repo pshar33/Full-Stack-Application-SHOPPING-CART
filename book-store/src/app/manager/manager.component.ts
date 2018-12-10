b875ec345b5e216fb3ca460b77a151945efe69ac
Parth Sharma <pshar33@uwo.ca>
2018-12-10 17:17:26 -0500

    Implemented manager login functionality

--- book-store/src/app/manager/manager.component.ts
+++ book-store/src/app/manager/manager.component.ts
@@ -2,31 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { BookService   } from '../book.service';
 import { NgForm } from '@angular/forms'
 @Component({
-  selector: 'app-manager',
+  selector: 'app-manager',       
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
 export class ManagerComponent implements OnInit {
-
+      
   constructor(private bookService: BookService) { }
-
+            
   ngOnInit() {
   this.resetForm();
-  }
+  }     
 
-  onSubmit(bookForm : NgForm){
+  onSubmit(bookForm : NgForm){       
   
-  }
+  }     
 
   resetForm(bookForm? : NgForm){
-  if(bookForm != null){
-  bookForm.reset();
-  this. bookService.selectedBook = {
+  if(bookForm != null){    
+  bookForm.reset();      
+  this. bookService.selectedBook = {   
     $key : null,
-    name : '',
+    name : '',   
     description : '',
-    availability : 0,
-    quantity : 0,
+    availability : 0,   
+    quantity : 0,          
     price : 0
 
 
